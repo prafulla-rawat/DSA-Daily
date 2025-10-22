0bf72f745d6bed17192baed0e782eabfe485bbfa
prafulla-rawat <prafullasureshrawat@gmail.com>
2025-10-22 22:37:32 +0530

    My first program

---
+++ First.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Please enter your Name";
+    string name;
+    cin << name;
+    cout << "Thank you " << name << endl;
+
+    return 0;
+}
